9c43394de65a43e97afcabeed03eb8eb0fa0de8d
emanmahdy <eman.mahdy232@gmail.com>
2023-04-25 16:50:16 -0700

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,23 +3,4 @@
 /**
  * print_to_98 - function that prints all natural numbers from n to 98
  *@n: input
- * Return: Always 0.
- */
-void print_to_98(int n)
-{
-int x;
-if (n > 98)
-{
-for (x = n; x  > 98; x++)
-{
-printf("%d", x);
-}
-}
-else
-{
-for (x = n; x < 98; x++)
-{
-printf("%d", x);
-}
-}
-}
+ * Return: Always 0
