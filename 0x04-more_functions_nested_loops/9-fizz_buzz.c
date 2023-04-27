2a8cfaa7af002960678d81e05e976f68ec472f5d
emanmahdy <eman.mahdy232@gmail.com>
2023-04-27 14:53:17 -0700

    M

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,6 +11,7 @@ int main(void)
 int i;
 
 for (i = 1; i <= 100; i++)
+{
 if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
 else if (i % 3 == 0)
@@ -22,6 +23,6 @@ printf("%d", i);
 if (i != 100)
 printf(" ");
 printf("\n");
-
+}
 return (0);
 }
