a0159099bac5d6880665c25ade95341dcc4afd6d
emanmahdy <eman.mahdy232@gmail.com>
2023-05-02 13:25:09 -0700

    M

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
 * _strlen - function that returns the length of a string
-* @s: string parameter input 
+* @s: string parameter input
 * Return: lenght of string
 */
 int _strlen(char *s)
