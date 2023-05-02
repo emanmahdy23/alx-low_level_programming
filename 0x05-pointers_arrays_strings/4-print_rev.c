d0eb0f602530a5c5cf419b3f291b6ded4b608aad
emanmahdy <eman.mahdy232@gmail.com>
2023-05-02 13:39:09 -0700

    M

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*  print_rev - function that prints a string in reverse
+* @s: the pointer to the string
+*/
+void print_rev(char *s)
+{
+int num = 0;
+while (s[num])
+{
+num++
+}
+while (num--)
+{
+_putchar(s[num]);
+}
+_putchar('\n');
+}
