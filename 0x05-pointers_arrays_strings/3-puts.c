b7930df86c8688fc289c9e65cfe02f02a938500a
emanmahdy <eman.mahdy232@gmail.com>
2023-05-02 13:32:27 -0700

    M

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*  _puts - function that prints a string
+* @str: string parametr
+*/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
