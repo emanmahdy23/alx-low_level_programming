ed45a185bc6a4e9939baf4965774936db596c22b
emanmahdy <eman.mahdy232@gmail.com>
2023-05-04 15:18:21 -0700

    M

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* cap_string - Capitalizes all words of a string.
+* @str: The string to be capitalized.
+* Return: A pointer to the changed string.
+*/
+int isLower(char c)
+{
+return (c >= 97 && c <= 122);
+}
+int isDelimiter(char c)
+{
+int i;
+char delimiter[] = " \t\n,.!?\"(){}";
+for (i = 0; i < 12; i++)
+	if (c == delimiter[i])
+		return (1);
+return (0);
+}
+char *cap_string(char *str)
+{
+char *ptr = s;
+int foundDelimit = 1;
+while (*s)
+if (isDelimiter(*s))
+foundDelimit = 1;
+else if (isLower(*s) && foundDelimit)
+*s -= 32;
+foundDelimit = 0;
+else
+foundDelimit = 0;
+s++;
+return (ptr);
+}
